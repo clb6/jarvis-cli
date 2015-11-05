0a5de8925e402b645f03c400415706c0d63d72e2
Michael Hwang <hirehwang@gmail.com>
2015-11-04 23:11:55 -0500

    Move metadata writing to a helper method

--- bin/jarvis.py
+++ bin/jarvis.py
@@ -21,10 +21,16 @@ if "__main__" == __name__:
         env_author = os.environ['JARVIS_AUTHOR']
         env_version = "0.1.0"
 
+        created = datetime.utcnow().replace(microsecond=0)
+
+        def write_metadata(f):
+            f.write("Author: {0}\n".format(env_author))
+            f.write("Created: {0}\n".format(created.isoformat()))
+            f.write("Version: {0}\n".format(env_version))
+            f.write("Tags: \n")
+
         if args.type == 'entry':
             dir_jarvis_logs = "{0}/LogEntries".format(env_dir_jarvis_root)
-
-            created = datetime.utcnow().replace(microsecond=0)
 
             # datetime.fromtimestamp(0) is not Unix epoch and returns
             # 1969-12-31 19:00 instead.
@@ -33,12 +39,10 @@ if "__main__" == __name__:
             filepath = "{0}/{1}.md".format(dir_jarvis_logs,
                     int((created - epoch).total_seconds()))
 
-        with open(filepath, 'w') as f:
-            f.write("Author: {0}\n".format(env_author))
-            f.write("Created: {0}\n".format(created.isoformat()))
-            f.write("Version: {0}\n".format(env_version))
-            f.write("Tags: \n")
+            with open(filepath, 'w') as f:
+                write_metadata(f)
 
         subprocess.call(["vim", filepath])
+        print("Created: {0}".format(filepath))
     else:
         print('DOH')
