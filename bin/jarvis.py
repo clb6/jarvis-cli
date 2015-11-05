76f3b059c5056e2c34bf5677a7318b5cd69f22ed
Michael Hwang <hirehwang@gmail.com>
2015-11-04 23:46:23 -0500

    Consolidate file creation functionality more

--- bin/jarvis.py
+++ bin/jarvis.py
@@ -12,6 +12,7 @@ if "__main__" == __name__:
             dest='element_type')
     parser_new_log = subparsers_new.add_parser('log', help='Create a new log entry')
     parser_new_tag = subparsers_new.add_parser('tag', help='Create a new tag element')
+    parser_new_tag.add_argument('tag_name', help='Tag name')
 
     args = parser.parse_args()
 
@@ -29,26 +30,28 @@ if "__main__" == __name__:
 
         created = datetime.utcnow().replace(microsecond=0)
 
-        def write_metadata(f):
-            f.write("Author: {0}\n".format(env_author))
-            f.write("Created: {0}\n".format(created.isoformat()))
-            f.write("Version: {0}\n".format(env_version))
-            f.write("Tags: \n")
+        def create_stub_file(subdir_name, filename):
+            dir_target = "{0}/{1}".format(env_dir_jarvis_root, subdir_name)
+
+            filepath = "{0}/{1}.md".format(dir_target, filename)
+
+            with open(filepath, 'w') as f:
+                f.write("Author: {0}\n".format(env_author))
+                f.write("Created: {0}\n".format(created.isoformat()))
+                f.write("Version: {0}\n".format(env_version))
+                f.write("Tags: \n")
+
+            return filepath
 
         if args.element_type == 'log':
-            dir_jarvis_logs = "{0}/LogEntries".format(env_dir_jarvis_root)
-
             # datetime.fromtimestamp(0) is not Unix epoch and returns
             # 1969-12-31 19:00 instead.
             epoch = datetime(1970, 1, 1)
 
-            filepath = "{0}/{1}.md".format(dir_jarvis_logs,
-                    int((created - epoch).total_seconds()))
-
-            with open(filepath, 'w') as f:
-                write_metadata(f)
+            filepath = create_stub_file('LogEntries',
+                int((created - epoch).total_seconds()))
         elif args.element_type == 'tag':
-            filepath = None
+            filepath = create_stub_file('Tags', args.tag_name)
             print("TAG")
         else:
             raise NotImplementedError("Unknown information type: {0}"
