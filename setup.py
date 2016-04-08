009e4c5f1cbfa6bc2bae01c5dec5d3b4df025164
Michael Hwang <hirehwang@gmail.com>
2016-04-07 23:03:29 -0400

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name = "jarvis", version = "0.2.0", packages = find_packages(),
+setup(name = "jarvis", version = "0.3.0", packages = find_packages(),
         author = "Michael Hwang", author_email = "hirehwnag@gmail.com",
         description = ("Jarvis python client"), scripts=['bin/jarvis.py'],
         install_requires=['requests', 'tabulate'], zip_safe = False)
