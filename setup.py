edbb9e2300b74cd78202c349279cdfbb62e4b05f
Michael Hwang <hirehwang@gmail.com>
2016-05-05 23:24:56 -0400

    Add jarvis_cli module and move version there

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
+from jarvis_cli import __version__
 
-setup(name = "jarvis", version = "0.4.0", packages = find_packages(),
+setup(name = "jarvis-cli", version = __version__, packages = find_packages(),
         author = "Michael Hwang", author_email = "hirehwnag@gmail.com",
         description = ("Jarvis python client"), scripts=['bin/jarvis.py'],
         install_requires=['requests', 'tabulate'], zip_safe = False)
