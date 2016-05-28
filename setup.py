d0b1cf3f61254f7f5d8026567453102c67c95a58
Michael Hwang <hirehwang@gmail.com>
2016-05-28 03:00:20 -0400

    Rework setup.py, install jarvis_migrate.py

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, find_packages
 from jarvis_cli import __version__
 
-setup(name = "jarvis-cli", version = __version__, packages = find_packages(),
-        author = "Michael Hwang", author_email = "hirehwnag@gmail.com",
-        description = ("Jarvis python client"), scripts=['bin/jarvis.py'],
-        install_requires=['requests', 'tabulate'], zip_safe = False)
+setup(
+        name = "jarvis-cli",
+        version = __version__,
+        packages = find_packages(),
+        author = "Michael Hwang",
+        author_email = "hirehwnag@gmail.com",
+        description = ("Jarvis command-line tool"),
+        scripts=['bin/jarvis.py', 'bin/jarvis_migrate.py'],
+        install_requires=['requests', 'tabulate'],
+        zip_safe = False
+        )
