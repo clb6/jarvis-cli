6482303cee3c6678cfed1f3a22dccd3758093c12
Michael Hwang <hirehwang@gmail.com>
2016-06-15 23:45:29 -0400

    Expand config to include jarvis data directories

--- jarvis_cli/config.py
+++ jarvis_cli/config.py
@@ -3,13 +3,26 @@ import configparser
 from jarvis_cli.exceptions import JarvisCliConfigError
 from jarvis_cli.client import DBConn
 
+JARVIS_CLI_DIRECTORY = os.path.join(os.environ["HOME"], ".jarvis")
 
-def get_client_connection(environment):
+
+def _get_config(environment):
     config = configparser.ConfigParser()
-    config_path = os.path.join(os.environ["HOME"], ".jarvis", "cli_config.ini")
+    config_path = os.path.join(JARVIS_CLI_DIRECTORY, "cli_config.ini")
 
     if config.read(config_path):
-        return DBConn(config[environment]["host"], config[environment]["port"])
+        return config[environment]
     else:
         raise JarvisCliConfigError("Configuration not setup: {0}".format(config_path))
 
+def get_client_connection(environment):
+    config = _get_config(environment)
+    return DBConn(config["host"], config["port"])
+
+def get_jarvis_data_directory(environment):
+    config = _get_config(environment)
+    return config["data_directory"]
+
+def get_jarvis_snapshots_directory(environment):
+    config = _get_config(environment)
+    return config["snapshots_directory"]
